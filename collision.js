b88ba5b33c4fd2dc2f5bffe5058ee876c27f5f22
David Holzapfel <delbon93@gmail.com>
2021-04-14 17:42:49 +0200

    Very minor improvement to collision detection

--- collision.js
+++ collision.js
@@ -4,15 +4,19 @@ function getEdgeCollision(graph, x, y, distanceThreshold, margin = 0) {
     result = {hit: false, distance: Infinity, closestPoint: null, fromNode: null, toNode: null};
     graph.nodes.forEach(node => {
         node.out.forEach(to => {
-            if (result.hit) return;
-            let dir = p5.Vector.sub(to.pos, node.pos);
-            let edgeLength = dir.mag();
-            dir.normalize();
-            let lambda = clamp(p5.Vector.dot(p5.Vector.sub(p, node.pos), dir), margin, edgeLength - margin);
-            let closest = p5.Vector.add(p5.Vector.mult(dir, lambda), node.pos);
-            let distanceToClosest = p5.Vector.sub(p, closest).mag();
-            if (distanceToClosest < distanceThreshold) {
-                result = {hit: true, distance: distanceToClosest, closestPoint: closest, fromNode: node, toNode: to};
+            let x1 = min(to.pos.x, node.pos.x); let y1 = min(to.pos.y, node.pos.y);
+            let x2 = max(to.pos.x, node.pos.x); let y2 = max(to.pos.y, node.pos.y);
+            if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+                let dir = p5.Vector.sub(to.pos, node.pos);
+                let edgeLength = dir.mag();
+                dir.normalize();
+                let lambda = clamp(p5.Vector.dot(p5.Vector.sub(p, node.pos), dir), margin, edgeLength - margin);
+                let closest = p5.Vector.add(p5.Vector.mult(dir, lambda), node.pos);
+                let distanceToClosest = p5.Vector.sub(p, closest).mag();
+                if (distanceToClosest < distanceThreshold) {
+                    result = {hit: true, distance: distanceToClosest, closestPoint: closest, fromNode: node, toNode: to};
+                    return;
+                }
             }
         });
         if (result.hit) return;
