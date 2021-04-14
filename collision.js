9f99b7134fb6b4471686d08e5e4cbdd18d32df30
David Holzapfel <delbon93@gmail.com>
2021-04-14 09:37:09 +0200

    Collision shape objects for circles and capsules

--- collision.js
+++ collision.js
@@ -1,3 +1,32 @@
+function createCircleCollider(r) {
+    return {
+        r: r,
+        point: function(offsetX, offsetY, x, y) {
+            let dx = x - offsetX;
+            let dy = y - offsetY;
+            return (dx * dx + dy * dy) <= this.r * this.r;
+        }
+    };
+}
+
+function createCapsuleCollider(x1, y1, x2, y2, r) {
+    return {
+        p1: createVector(x1, y1), p2: createVector(x2, y2), r: r,
+        point: function(offsetX, offsetY, x, y) {
+            let v = createVector(x, y);
+            let offset = createVector(offsetX, offsetY);
+            let from = p5.Vector.add(this.p1, offset);
+            let to = p5.Vector.add(this.p2, offset);
+            let dir = p5.Vector.sub(from, to);
+            let edgeLength = dir.mag();
+            dir.normalize();
+            let lambda = clamp(p5.Vector.dot(p5.Vector.sub(v, from), dir), 0, edgeLength);
+            let closest = p5.Vector.add(p5.Vector.mult(dir, lambda), from);
+            let distanceToClosest = p5.Vector.sub(p, closest).mag();
+            return distanceToClosest <= this.r;
+        }
+    };
+}
 
 function getEdgeCollision(graph, x, y, distanceThreshold, margin = 0) {
     let p = createVector(x, y);
